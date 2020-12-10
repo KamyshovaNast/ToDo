125089c2f9f32915b4e38b87cc2100e969896718
Kamyshova Anastasia <kamyshova.nast@gmail.com>
2020-12-10 20:07:44 +0300

    Light-purple colours. Todo works

---
+++ todolist/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>Дела на сегодня:</h1>
+    <TodoList v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      todos: ['Проснуться и прочесть письма', 'Завтрак', 'Прогулка', 'Ресторан', 'Театр', 'Гости', 'Сон']
+    }
+  },
+  components: {
+    TodoList
+  },
+  methods: {
+    deleteTodo(index){
+      this.todos.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style>
+    #app {
+        width: 700px;
+        margin: auto;
+        font-family: 'Arial', sans-serif;
+        font-size: 18px;
+        color:#35495e;
+    }
+</style>
